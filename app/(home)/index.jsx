62315537d8c7927c8d0851d569f28ac5d52ae7d0
Karthikeyan <165583967+Karthikeyan8296@users.noreply.github.com>
2024-10-02 09:19:26 +0530

    HomeScreen with option selected

--- app/(home)/index.jsx
+++ app/(home)/index.jsx
@@ -1,21 +1,89 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useState } from "react";
 
 const index = () => {
+  const [Option, setOption] = useState("Today");
+
   return (
-    <View style={styles.container}>
-      <Text>Index of home</Text>
-    </View>
+    <ScrollView style={{ flex: 1, backgroundColor: "white", padding: 10 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingHorizontal: 8,
+        }}
+      >
+        <Ionicons name="logo-bitbucket" size={30} color="black" />
+        <AntDesign name="plus" size={24} color="black" />
+      </View>
+
+      <Text
+        style={{
+          marginTop: 6,
+          fontSize: 26,
+          fontWeight: "500",
+          paddingHorizontal: 8,
+        }}
+      >
+        Habits
+      </Text>
+
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 10,
+          marginVertical: 10,
+        }}
+      >
+        <Pressable
+          onPress={() => setOption("Today")}
+          style={{
+            backgroundColor: Option == "Today" ? "#E0E0E0" : "transparent",
+            paddingHorizontal: 10,
+            paddingVertical: 8,
+            borderRadius: 24,
+          }}
+        >
+          <Text style={{ textAlign: "center", color: "grey", fontSize: 14 }}>
+            Today
+          </Text>
+        </Pressable>
+        <Pressable
+          onPress={() => setOption("weekly")}
+          style={{
+            backgroundColor: Option == "weekly" ? "#E0E0E0" : "transparent",
+            paddingHorizontal: 10,
+            paddingVertical: 8,
+            borderRadius: 24,
+          }}
+        >
+          <Text style={{ textAlign: "center", color: "grey", fontSize: 14 }}>
+            Weekly
+          </Text>
+        </Pressable>
+        <Pressable
+          onPress={() => setOption("Over all")}
+          style={{
+            backgroundColor: Option == "Over all" ? "#E0E0E0" : "transparent",
+            paddingHorizontal: 10,
+            paddingVertical: 8,
+            borderRadius: 24,
+          }}
+        >
+          <Text style={{ textAlign: "center", color: "grey", fontSize: 14 }}>
+            Over all
+          </Text>
+        </Pressable>
+      </View>
+    </ScrollView>
   );
 };
 
 export default index;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
